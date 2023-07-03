a54420c0046b567aa062ab3060d81ab042b13a95
Basil Victor <basilbennedict@gmail.com>
2023-07-03 19:01:05 +0530

    Created login form with bootstrap styling

--- README.md
+++ README.md
@@ -10,5 +10,5 @@ By running `npm run build`, the production deployable project is built.
 ## Git Repository
 The training has been split into a few checkpoints, the code base at each of these checkpoints can be found in their own branch. The branch follow a naming convention like `<checkpoint-number>_<breif-description>`. The `master` branch contains the complete latest code.
 
-## 2_login-page
-Here we create a placeholder login page and configure the router to show the login page.
+## 3_styling-form
+Here we create the complete login form and also bind a button click event that prints the entered email and password.
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,36 @@
+import { useState } from 'react'
+
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = () => {
+        console.log("Email: " + email);
+        console.log("Password: " + password);
+    }
+
+    return (
+        <>
+            <div className="container">
+                <div className="d-flex justify-content-center">
+                    <div className="col-12 col-md-6">
+                        <div className="rounded shadow p-4 mt-4">
+                            <h3>Login</h3>
+                            <hr></hr>
+                            <div className="mb-3">
+                                <label className="form-label">Email</label>
+                                <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Password</label>
+                                <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
+                            </div>
+                            <button type="button" className="btn btn-primary" onClick={handleLogin}>Login</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
 }
